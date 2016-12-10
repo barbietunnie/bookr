95aeb6cbc8da74f18f3ac8d6a1abe8488d87971b
Babatunde Adeyemi <barbietunnie@gmail.com>
2016-12-10 18:18:13 +0100

    Implement store method and dump exception to show mass assignment isn't allowed

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Book;
+use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 /**
@@ -40,5 +41,21 @@ class BooksController
             ]
         ], 404);
     }
+  }
+
+  /**
+   * POST /books
+   * @param Request $request
+   * @return \Symfony\Component\HttpFoundation\Response
+   */
+  public function store(Request $request)
+  {
+      try {
+        $book = Book::create($request->all());
+      } catch(\Exception $e) {
+        dd(get_class($e));
+      }
+
+      return response()->json(['created' => true], 201);
   }
 }
