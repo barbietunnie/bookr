28c8c30b9439286da602384e60f53d8e77b99f86
Babatunde Adeyemi <barbietunnie@gmail.com>
2016-12-20 14:10:45 +0100

    Specifically catch ModelNotFoundException and return the proper error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -55,6 +55,9 @@ class Handler extends ExceptionHandler
             if($e instanceof HttpException) {
                 $response['message'] = Response::$statusTexts[$e->getStatusCode()];
                 $response['status'] = $e->getStatusCode();
+            } else if($e instanceof ModelNotFoundException) {
+                $response['message'] = Response::$statusTexts[Response::HTTP_NOT_FOUND];
+                $response['status'] = Response::HTTP_NOT_FOUND;
             }
 
             if($this->isDebugMode()) {
